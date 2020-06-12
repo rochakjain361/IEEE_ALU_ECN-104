b6d2b92b1061ee1b5aa555eb2b4c62ffb74d51a6
rochakjain361 <rochaktitanium2@gmail.com>
2020-06-13 03:20:16 +0530

    Minor Changes

--- ALU.v
+++ ALU.v
@@ -61,20 +61,20 @@ module ALU(
     
     Alu_addAndSubtract ASI(Add_Sub_A, Add_Sub_B, AddBar_Sub, Add_Sub_Exception, Add_Sub_Output);
     
-    Alu_multiplication MuI(Mul_A, Mul_B, Mul_Overflow, Mul_Underflow, Mul_Output);
+    Alu_multiplication MuI(Mul_A, Mul_B, Mul_Exception, Mul_Overflow, Mul_Underflow, Mul_Output);
     
-    Alu_division DiI(Div_A, Div_B, Exception, Div_Output);
+    Alu_division DiI(Div_A, Div_B, Div_Exception, Div_Output);
     
     assign Logical_Operations_Exception = (&operand_a[30:23] | &operand_b[30:23]) ? 1'b1 : 1'b0 ;
     
     assign {exception, overflow, underflow, alu_result} = (operation == 3'd1) ? {Add_Sub_Exception,1'b0,1'b0,Add_Sub_Output} : 35'dz; 
     assign {exception, overflow, underflow, alu_result} = (operation == 3'd2) ? {Add_Sub_Exception,1'b0,1'b0,Add_Sub_Output} : 35'dz; 
-    assign {exception, overflow, underflow, alu_result} = (operation == 3'd3) ? {Mul_Exception,Mul_Overflow,Mul_Underflow,Mul_Output} : 35'dz;
+    assign {exception, overflow, underflow, alu_result} = (operation == 3'd3) ? {Mul_Exception, Mul_Overflow, Mul_Underflow, Mul_Output} : 35'dz;
     assign {exception, overflow, underflow, alu_result} = (operation == 3'd4) ? {Div_Exception, 1'b0, 1'b0, Div_Output} : 35'dz;
     assign {exception, overflow, underflow, alu_result} = (operation == 3'd5) ? {Logical_Operations_Exception, 1'b0, 1'b0, OR_Output} : 35'dz;
     assign {exception, overflow, underflow, alu_result} = (operation == 3'd6) ? {Logical_Operations_Exception, 1'b0, 1'b0, AND_Output} : 35'dz;
     assign {exception, overflow, underflow, alu_result} = (operation == 3'd7) ? {Logical_Operations_Exception, 1'b0, 1'b0, NOT_Output} : 35'dz;
-
-
+    
+    
     
 endmodule
